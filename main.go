6b4ece0a66acba1fa156467b40a974739aa42296
Pradhan Sagar K <pradhansagark2005@gmail.com>
2025-04-06 17:38:41 +0530

    added .gitignore

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+
+	fmt.Println("Port : ", port)
+}
